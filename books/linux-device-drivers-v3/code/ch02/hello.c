94b60bc69ebb1c4a2383c9eed70f64b25be55fd7
pkemb <pk@pkemb.com>
2020-10-12 22:33:02 +0800

    完善 hello world 模块

--- books/linux-device-drivers-v3/code/ch02/hello.c
+++ books/linux-device-drivers-v3/code/ch02/hello.c
@@ -1,16 +1,22 @@
-#include <linux/init.h>
-#include <linux/module.h>
-MODULE_LICENSE("Dual BSD/GPL");
+#include <linux/init.h>    // 指定初始化和清除函数
+#include <linux/module.h>  // 含有可装载模块需要的大量符合和函数的定义
+#include <linux/kernel.h>  // printk
 
-static int hello_init(void)
+static int __init hello_init(void)
 {
     printk(KERN_ALERT "hello world!\n");
 }
+module_init(hello_init);
 
-static int hello_exit(void)
+static void __exit hello_exit(void)
 {
     printk(KERN_ALERT "Googbye hello world!\n");
 }
-
-module_init(hello_init);
 module_exit(hello_exit);
+
+MODULE_LICENSE("Dual BSD/GPL");         // 代码使用的许可证
+MODULE_AUTHOR("pk");                    // 模块作者
+MODULE_DESCRIPTION("hello world");      // 说明模块用途的简短描述
+MODULE_VERSION("0.1");                  // 版本
+MODULE_ALIAS("helloworld");             // 模块别名
+// MODULE_DEVICE_TABLE()
