05c1a9c992f4e32f95531cffddacd9a34e3b8249
pkemb <pkemb@outlook.com>
2021-09-04 10:39:32 +0800

    完善字符设备的框架，添加及删除字符设备，fops结构体
    Change-Id: Ia9488c576c3170f68eaf64f581a593e7b803be1b

--- books/linux-device-drivers-v3/code/pkchr/pkchr.c
+++ books/linux-device-drivers-v3/code/pkchr/pkchr.c
@@ -2,16 +2,102 @@
 #include <linux/init.h>
 #include "pkchr.h"
 
+static int pkchr_major = 0;
+static int pkchr_minor = 0;
+static int pkchr_dev_num = 1;
+
+struct pkchr_dev pkchr = {0};
+
+// inode 表示一个唯一的文件，主要关注成员 i_rdev 和 i_cdev
+// filp 表示一个打开的文件，f_mode / f_ops / f_flags / f_op / private_data / f_dentry
+int pkchr_open(struct inode *inode, struct file *filp)
+{
+    return 0;
+}
+
+ssize_t pkchr_write(struct file *filp, const char __user *buff, size_t size, loff_t *off)
+{
+    return 0;
+}
+
+ssize_t pkchr_read(struct file *filp, char __user *buff, size_t size, loff_t *off)
+{
+    return 0;
+}
+
+loff_t pkchr_llseek(struct file *filp, loff_t off, int whence)
+{
+    return 0;
+}
+
+int pkchr_ioctl(struct inode *inode, struct file *filp, unsigned int cmd, unsigned long arg)
+{
+    return 0;
+}
+
+// file结构释放时，将调用此函数
+int pkchr_release(struct inode *inode, struct file *filp)
+{
+    return 0;
+}
+
+struct file_operations fops = {
+    .owner   = THIS_MODULE,
+    .open    = pkchr_open,
+    .write   = pkchr_write,
+    .read    = pkchr_read,
+    .llseek  = pkchr_llseek,
+    .ioctl   = pkchr_ioctl,
+    .release = pkchr_release,
+};
+
 static int __init pkchr_init(void)
 {
-    printk(KERN_INFO"hello\n");
+    dev_t devno = 0;
+    int ret = 0;
+    printk(KERN_INFO"pk char device init\n");
+
+    // 申请设备号，0 正确，小于0 错误
+    ret = alloc_chrdev_region(
+        &devno,         // 设备号
+        0,              // 第一个子设备
+        pkchr_dev_num,  // 设备总数
+        DEVICE_NAME);   // 设备名
+    if (ret < 0) {
+        printk(KERN_ERR"dev number alloc fail, ret = %d\n", ret);
+        return -1;
+    }
+    printk(KERN_INFO"devno = %d\n", devno);
+    pkchr_major = MAJOR(devno);
+    pkchr_minor = MINOR(devno);
+    printk(KERN_INFO"major = %d\n", pkchr_major);
+    printk(KERN_INFO"minor = %d\n", pkchr_minor);
+
+    // 初始化 pkchr 变量
+    // 初始化 struct cdev, 需要 fops 结构体
+    cdev_init(&pkchr.cdev, &fops);
+    pkchr.cdev.owner = THIS_MODULE;
+    pkchr.cdev.ops   = &fops;
+
+    // 注册cdev
+    ret = cdev_add(&pkchr.cdev, devno, 1);
+    if (ret < 0) {
+        printk(KERN_INFO"add cdev %x fail, err = %d\n", devno, ret);
+        return -1;
+    }
+
     return 0;
 }
 module_init(pkchr_init);
 
 static void __exit pkchr_exit(void)
 {
-    printk(KERN_INFO"exit\n");
+    dev_t dev = MKDEV(pkchr_major, pkchr_minor);
+    printk(KERN_INFO"pk char device exit\n");
+    // 删除字符设备
+    cdev_del(&pkchr.cdev);
+    // 删除设备号
+    unregister_chrdev_region(dev, pkchr_dev_num);
 }
 module_exit(pkchr_exit);
 
--- books/linux-device-drivers-v3/code/pkchr/pkchr.h
+++ books/linux-device-drivers-v3/code/pkchr/pkchr.h
@@ -1,5 +1,16 @@
 #ifndef __PKCHR_H__
 #define __PKCHR_H__
 
+#include <linux/cdev.h>
+#include <linux/types.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+
+#define DEVICE_NAME     "pkchr"
+
+struct pkchr_dev {
+    struct cdev cdev;
+    // 设备自定义数据
+};
 
 #endif // __PKCHR_H__
