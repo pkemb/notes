dadc775df8cdc53ad2accee32dce3703b792f220
pkemb <pkemb@outlook.com>
2021-10-17 13:52:30 +0800

    pktime, 添加/proc/pktime_busy, pktime_sched, pktime_queue, pktime_schedto 测试命令：dd bs=20 count=5 < /proc/pktime_busy，其他文件类型 pktime_busy和pktime_sched是死等，会消耗整个CPU pktime_queue和pktime_schedto是基于等待队列，不会消耗CPU 注意，使用head测试会导致进程无法正常退出
    Signed-off-by: pkemb <pkemb@outlook.com>
    Change-Id: I3aed15d99f14565e92b8ca13e5d8c9c8b3e218b6

--- books/linux-device-drivers-v3/code/pktime/pktime.c
+++ books/linux-device-drivers-v3/code/pktime/pktime.c
@@ -6,11 +6,16 @@
 #include <linux/proc_fs.h>
 #include <linux/jiffies.h>
 #include <linux/timex.h>
+#include <linux/sched.h>
 
 #include "pktime.h"
 
 struct proc_dir_entry *proc_jiffies = NULL;
 struct proc_dir_entry *proc_cycles  = NULL;
+struct proc_dir_entry *proc_busy    = NULL;
+struct proc_dir_entry *proc_sched   = NULL;
+struct proc_dir_entry *proc_queue   = NULL;
+struct proc_dir_entry *proc_schedto = NULL;
 
 int pktime_jiffies(char *buf, char **start, off_t offset, int count, int *eof, void *data)
 {
@@ -65,6 +70,48 @@ int pktime_cycles(char *buf, char **start, off_t offset, int count, int *eof, vo
     return len;
 }
 
+int pktime_delay(char *buf, char **start, off_t offset, int count, int *eof, void *data)
+{
+    long before = 0;
+    long after  = 0;
+    long delay  = S2J(1);
+    int len = 0;
+    wait_queue_head_t wait;
+
+    if (!try_module_get(THIS_MODULE))
+        return 0;
+
+    init_waitqueue_head(&wait);
+
+    before = jiffies;
+    after  = before + delay;
+
+    switch((int)data) {
+    case BUSY:
+        while (time_before(jiffies, after))
+            cpu_relax();
+        break;
+    case SCHED:
+        while (time_before(jiffies, after))
+            schedule();
+        break;
+    case QUEUE:
+        wait_event_interruptible_timeout(wait, 0, delay); // 第二个参数，唤醒条件，一直为false
+        break;
+    case SCHEDTO:
+        // 如果不更改进程状态，schedule_timeout()等同于schedule()
+        set_current_state(TASK_INTERRUPTIBLE);
+        schedule_timeout(delay);
+    }
+
+    after = jiffies;
+    len += sprintf(buf + len, "%9li %9li\n", before, jiffies);
+
+    *start = buf;
+    module_put(THIS_MODULE);
+    return len;
+}
+
 static int __init pktime_init(void)
 {
     PDEBUG("%s init\n", DEVICE_NAME);
@@ -82,11 +129,39 @@ static int __init pktime_init(void)
         goto create_proc_fail;
     }
 
+    proc_busy = create_proc_read_entry(PROC_BUSY, 0, NULL, pktime_delay, (void *)BUSY);
+    if (proc_busy == NULL) {
+        PDEBUG("create %s fail\n", PROC_BUSY);
+        goto create_proc_fail;
+    }
+
+    proc_sched = create_proc_read_entry(PROC_SCHED, 0, NULL, pktime_delay, (void *)SCHED);
+    if (proc_sched == NULL) {
+        PDEBUG("create %s fail\n", PROC_SCHED);
+        goto create_proc_fail;
+    }
+
+    proc_queue = create_proc_read_entry(PROC_QUEUE, 0, NULL, pktime_delay, (void *)QUEUE);
+    if (proc_queue == NULL) {
+        PDEBUG("create %s fail\n", PROC_QUEUE);
+        goto create_proc_fail;
+    }
+
+    proc_schedto = create_proc_read_entry(PROC_SCHEDTO, 0, NULL, pktime_delay, (void *)SCHEDTO);
+    if (proc_schedto == NULL) {
+        PDEBUG("create %s fail\n", PROC_SCHEDTO);
+        goto create_proc_fail;
+    }
+
     return 0;
 
 create_proc_fail:
     SAFE_REMOVE_PROC_ENTRY(proc_jiffies, PROC_JIFFIES);
     SAFE_REMOVE_PROC_ENTRY(proc_cycles,  PROC_CYCLES);
+    SAFE_REMOVE_PROC_ENTRY(proc_busy,    PROC_BUSY);
+    SAFE_REMOVE_PROC_ENTRY(proc_sched,   PROC_SCHED);
+    SAFE_REMOVE_PROC_ENTRY(proc_queue,   PROC_QUEUE);
+    SAFE_REMOVE_PROC_ENTRY(proc_schedto, PROC_SCHEDTO);
     return -1;
 }
 module_init(pktime_init);
@@ -98,6 +173,10 @@ static void __exit pktime_exit(void)
     // 删除proc入口
     SAFE_REMOVE_PROC_ENTRY(proc_jiffies, PROC_JIFFIES);
     SAFE_REMOVE_PROC_ENTRY(proc_cycles,  PROC_CYCLES);
+    SAFE_REMOVE_PROC_ENTRY(proc_busy,    PROC_BUSY);
+    SAFE_REMOVE_PROC_ENTRY(proc_sched,   PROC_SCHED);
+    SAFE_REMOVE_PROC_ENTRY(proc_queue,   PROC_QUEUE);
+    SAFE_REMOVE_PROC_ENTRY(proc_schedto, PROC_SCHEDTO);
 }
 module_exit(pktime_exit);
 
--- books/linux-device-drivers-v3/code/pktime/pktime.h
+++ books/linux-device-drivers-v3/code/pktime/pktime.h
@@ -12,6 +12,10 @@
 // proc name
 #define PROC_JIFFIES               "pktime_jiffies"
 #define PROC_CYCLES                "pktime_cycles"
+#define PROC_BUSY                  "pktime_busy"
+#define PROC_SCHED                 "pktime_sched"
+#define PROC_QUEUE                 "pktime_queue"
+#define PROC_SCHEDTO               "pktime_schedto"
 
 #define SAFE_REMOVE_PROC_ENTRY(entry, path)       \
      do {                                         \
@@ -20,6 +24,23 @@
                entry = NULL;                      \
           }                                       \
      } while(0)
+
+enum {
+     BUSY = 1,
+     SCHED,
+     QUEUE,
+     SCHEDTO,
+} e_delay;
+
+// jiffies和second 的互相转换
+// jiffies转换为s
+#define J2S(j)      (j/(HZ))
+// jiffies转换为ms
+#define J2MS(j)     ((j*1000)/(HZ))
+// s转换为jiffies
+#define S2J(s)      (s*(HZ))
+#define MS2J(ms)    ((ms*(HZ))/1000)
+
 
 #define DEBUG
 
