85f71ea3039400d0b812cdee3cebb809d5615296
pkemb <pkemb@outlook.com>
2021-12-15 23:57:22 +0800

    Linux设备驱动，添加uevent，实际没有生效
    Signed-off-by: pkemb <pkemb@outlook.com>
    Change-Id: I67ba564d2cbf3a002d7dc7af0469499ff172eb3d

--- books/linux-device-drivers-v3/code/pkobj/pkobj.c
+++ books/linux-device-drivers-v3/code/pkobj/pkobj.c
@@ -55,6 +55,7 @@ void kobj_release(struct kobject *kobj)
 struct attribute att = {
     .name = "test",
     .mode = 0666,
+    .owner = THIS_MODULE,
 };
 
 struct attribute att1 = {
@@ -78,11 +79,48 @@ struct sysfs_ops att_ops = {
     .store = att_store,
 };
 
+// 返回0，将不产生uevent事件
+int pkobj_uevent_filter(struct kset *kset, struct kobject *kobj)
+{
+    if (kset != NULL)
+        PDEBUG("kset name=%s\n", kset->kobj.name);
+    else
+        PDEBUG("kset is null\n");
+    if (kobj != NULL)
+        PDEBUG("kobj name=%s\n", kobj->name);
+    else
+        PDEBUG("kobj is null\n");
+    return (kobj == parent);
+}
+
+const char *pkobj_uevent_name(struct kset *kset, struct kobject *kobj)
+{
+    PDEBUG("pkobj_uevent_name\n");
+    return "pk";
+}
+
+int pkobj_uevent_uevent(struct kset *kset, struct kobject *kobj,
+		                struct kobj_uevent_env *env)
+{
+    PDEBUG("pkobj_uevent_uevent\n");
+    return 0;
+}
+
+struct kset_uevent_ops uevent_ops = {
+    .filter = pkobj_uevent_filter,
+    .name   = pkobj_uevent_name,
+    .uevent = pkobj_uevent_uevent,
+};
+
 static int __init pkobj_init(void)
 {
     int ret = 0;
-    kset = kset_create_and_add("pk", NULL, NULL);
-    if (kset == NULL) {
+    char *envp[2];
+    envp[0] = "PKOBJNAME=pk";
+    envp[1] = NULL;
+
+    kset = kset_create_and_add("pk", &uevent_ops, NULL);
+    if (!kset) {
         PDEBUG("create kset fail\n");
         goto kset_fail;
     }
@@ -97,6 +135,9 @@ static int __init pkobj_init(void)
     parent->ktype->sysfs_ops = &att_ops;
     parent->ktype->default_attrs = default_attrs;
     parent->ktype->release   = kobj_release;
+
+    kobject_uevent_env(parent, KOBJ_ADD, envp);
+
     ret = sysfs_create_file(parent, &att);
     if (ret != 0) {
         PDEBUG("create file fail, ret = %d\n", ret);
@@ -105,7 +146,7 @@ static int __init pkobj_init(void)
     return 0;
 
 fail_create_fail:
-    kobject_del(parent);
+    kobject_put(parent);
 fail_add:
     if (kset != NULL)
         kset_unregister(kset);
@@ -116,8 +157,9 @@ module_init(pkobj_init);
 
 static void __exit pkobj_exit(void)
 {
+    PDEBUG("pkobj_exit\n");
     sysfs_remove_file(parent, &att);
-    kobject_del(parent);
+    kobject_put(parent);
     kset_unregister(kset);
 }
 module_exit(pkobj_exit);
