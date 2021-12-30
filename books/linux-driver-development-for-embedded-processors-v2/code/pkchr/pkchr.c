8c75f84a4bd2d1d26de591f944241dbc8d899dcf
pkemb <pkemb@outlook.com>
2021-12-30 21:46:41 +0800

    嵌入式Linux设备驱动开发，完善pkchr，使用udev创建设备文件
    Signed-off-by: pkemb <pkemb@outlook.com>
    Change-Id: Ida1e1b09ab513b96c7f7580f8909ea81fedd36d1

--- books/linux-driver-development-for-embedded-processors-v2/code/pkchr/pkchr.c
+++ books/linux-driver-development-for-embedded-processors-v2/code/pkchr/pkchr.c
@@ -6,6 +6,7 @@
 #include <linux/uaccess.h>
 #include <linux/slab.h>
 #include <linux/ctype.h>
+#include <linux/device.h>
 
 MODULE_AUTHOR("pkemb");
 MODULE_LICENSE("GPL");
@@ -14,8 +15,9 @@ MODULE_DESCRIPTION("char device, create device node with udev");
 #define log(fmt, ...)   pr_debug("[%s:%d]" fmt, __FUNCTION__, __LINE__, ##__VA_ARGS__)
 
 struct pkchrdev {
-    char *name;
+    dev_t dev;
     struct cdev cdev;
+    struct device *device;
 };
 #define to_pkchrdev(ptr)     container_of(ptr, struct pkchrdev, cdev)
 
@@ -32,7 +34,8 @@ int pkchr_open(struct inode *inode, struct file *filp)
 ssize_t pkchr_read(struct file *filp, char __user *buff, size_t size, loff_t *off)
 {
     struct pkchrdev *pkchr = filp->private_data;
-    log("name = %s\n", pkchr->name);
+    dev_t dev = pkchr->dev;
+    log("dev = 0x%08x, major=%d, minor=%d\n", dev, MAJOR(dev), MINOR(dev));
     return size;
 }
 
@@ -48,8 +51,9 @@ ssize_t pkchr_write(struct file *filp, const char __user *buff, size_t size, lof
 {
     struct pkchrdev *pkchr = filp->private_data;
     char *ptr = NULL;
+    dev_t dev = pkchr->dev;
 
-    log("name = %s, size = %d, *pos = %p\n", pkchr->name, size, *off);
+    log("dev = 0x%08x, major=%d, minor=%d\n", dev, MAJOR(dev), MINOR(dev));
     ptr = kmalloc(size, GFP_KERNEL);
     log("ptr = %p\n", ptr);
     if (ptr) {
@@ -70,14 +74,17 @@ struct file_operations fops = {
     .open  = pkchr_open,
 };
 
-#define COUNT   1
+#define COUNT   4
 struct pkchrdev pkchrdevs[COUNT] = {};
+struct class   *pkclass = NULL;
 
 static int pkchr_init(void)
 {
     int ret = 0;
     int i = 0;
+    dev_t dev = 0;
 
+    // 最后一个参数设备名会出现在文件 /proc/devices
     ret = alloc_chrdev_region(&devno, 0, COUNT, MODULE_NAME);
     if (ret) {
         log("alloc char devno fail, ret = %d\n", ret);
@@ -85,13 +92,37 @@ static int pkchr_init(void)
     }
     log("major = %d, minor = %d\n", MAJOR(devno), MINOR(devno));
 
+    pkclass = class_create(THIS_MODULE, "pkclass");
+    if (IS_ERR(pkclass)) {
+        log("class create fail\n");
+        goto class_create_fail;
+    }
+
     for (i=0; i<COUNT; i++) {
-        pkchrdevs[i].name = MODULE_NAME;
+        dev = MKDEV(MAJOR(devno), i);
+        // 添加字符设备
+        pkchrdevs[i].dev = dev;
         cdev_init(&pkchrdevs[i].cdev, &fops);
-        cdev_add(&pkchrdevs[i].cdev, MKDEV(MAJOR(devno), i), 1);
+        cdev_add(&pkchrdevs[i].cdev, dev, 1);
+
+        // 创建device
+        pkchrdevs[i].device = device_create(pkclass, NULL, dev, NULL, MODULE_NAME"%d", i);
+        if (IS_ERR(pkchrdevs[i].device)) {
+            log("device %d create fail\n", i);
+            goto device_create_fail;
+        }
     }
 
     return 0;
+device_create_fail:
+    for (i=0; i<COUNT; i++) {
+        if (!IS_ERR(pkchrdevs[i].device)) {
+            device_destroy(pkclass, devno);
+        }
+    }
+    class_destroy(pkclass);
+class_create_fail:
+    unregister_chrdev_region(devno, COUNT);
 alloc_chrdev_region_fail:
     return ret;
 }
@@ -102,8 +133,12 @@ static void pkchr_exit(void)
     int i = 0;
     log("exit");
     for (i=0; i<COUNT; i++) {
+        device_destroy(pkclass, pkchrdevs[i].dev);
         cdev_del(&pkchrdevs[i].cdev);
     }
+
+    class_destroy(pkclass);
+
     if (devno) {
         unregister_chrdev_region(devno, COUNT);
         devno = 0;
