9472654996705f80d5cad0232037fc646c257a1f
pkemb@outlook.com <pkemb@outlook.com>
2022-03-26 18:23:14 +0800

    setenv.sh：修复TOOLDIR不存在时终端退出的错误
    Signed-off-by: pkemb@outlook.com <pkemb@outlook.com>
    Change-Id: Ia9c0a5ef6463edc16f44a89ed1d8dea2b20f045f

--- books/linux-device-drivers/rpi-kernel/setenv.sh
+++ books/linux-device-drivers/rpi-kernel/setenv.sh
@@ -1,12 +1,17 @@
 # 工具链根目录
 TOOLDIR=${HOME}/tools-rpi
 
-if [ ! -d "$TOOLDIR" ]; then
-    echo "$TOOLDIR not exits, please check"
-    exit -1
-fi
+function main()
+{
+    if [ ! -d "$TOOLDIR" ]; then
+        echo "$TOOLDIR not exits, please check"
+        return 1
+    fi
 
-export PATH=${TOOLDIR}/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin:$PATH
-export TOOLCHAIN=${TOOLDIR}/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64
-export CROSS_COMPILE=arm-linux-gnueabihf-
-export ARCH=arm
+    export PATH=${TOOLDIR}/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin:$PATH
+    export TOOLCHAIN=${TOOLDIR}/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64
+    export CROSS_COMPILE=arm-linux-gnueabihf-
+    export ARCH=arm
+}
+
+main
